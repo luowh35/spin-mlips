e5a5243a7937a4f84439dba34b7c372f22051192
luowh35 <luowh35@mail2.sysu.edu.cn>
2026-01-26 21:46:15 +0800

    fix bug

--- interface/USER-NEP-SPIN/Install.sh
+++ interface/USER-NEP-SPIN/Install.sh
@@ -7,11 +7,25 @@ if (test $1 = 1) then
 
   # Install: copy files to src directory
 
-  # Base files (always installed)
+  # Pair style
   cp pair_nep_spin.cpp ..
   cp pair_nep_spin.h ..
+
+  # NEP core files
   cp nep_spin_data.cpp ..
   cp nep_spin_data.h ..
+  cp nep_types.h ..
+  cp descriptor.cpp ..
+  cp descriptor.h ..
+  cp math_utils.cpp ..
+  cp math_utils.h ..
+  cp cg_coefficients.h ..
+  cp model.cpp ..
+  cp model.h ..
+  cp neighbor_list.cpp ..
+  cp neighbor_list.h ..
+  cp xyz_reader.cpp ..
+  cp xyz_reader.h ..
 
   # RK4 integrator (alternative to SIB)
   cp fix_nve_spin_rk4.cpp ..
@@ -29,11 +43,25 @@ elif (test $1 = 0) then
 
   # Uninstall: remove files from src directory
 
-  # Base files
+  # Pair style
   rm -f ../pair_nep_spin.cpp
   rm -f ../pair_nep_spin.h
+
+  # NEP core files
   rm -f ../nep_spin_data.cpp
   rm -f ../nep_spin_data.h
+  rm -f ../nep_types.h
+  rm -f ../descriptor.cpp
+  rm -f ../descriptor.h
+  rm -f ../math_utils.cpp
+  rm -f ../math_utils.h
+  rm -f ../cg_coefficients.h
+  rm -f ../model.cpp
+  rm -f ../model.h
+  rm -f ../neighbor_list.cpp
+  rm -f ../neighbor_list.h
+  rm -f ../xyz_reader.cpp
+  rm -f ../xyz_reader.h
 
   # RK4 integrator
   rm -f ../fix_nve_spin_rk4.cpp
